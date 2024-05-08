4c331247e96faa0653ce5e9a8af994e9571006c9
jeysongomez <jeysongomez17@hotmail.com>
2024-05-08 12:29:53 -0500

    Avanzes

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import './Redes.css'
+import "./Redes.css";
 
 function Redes() {
   return (
-
     <div class="social-sidebar">
-    <a href="https://github.com/JeysonGomez" className="social-icon"><img src="src/assets/GitHub.png" alt="" /></a>
-    <a href="https://www.linkedin.com/in/jeysongomez17/" className="social-icon"><img src="src/assets/Logolinkedin.png" alt="" /></a>
-</div>
+      <a href="https://github.com/JeysonGomez" className="social-icon">
+        <img src="src/assets/GitHub.png" alt="" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jeysongomez17/"
+        className="social-icon"
+      >
+        <img src="src/assets/Logolinkedin.png" alt="" />
+      </a>
+    </div>
   );
 }
 
