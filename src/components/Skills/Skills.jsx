4c37efe93f9ba748c55a1e61c5f3c81fa9c19183
JeysonGomez <jeysongomez17@hotmail.com>
2024-05-20 10:43:38 -0500

    Correciones JSX y CSS Skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,61 +10,61 @@ function Skills() {
         </div>
 
         <div className="imgSkills">
-          <div className="im1">
+          <div>
             <img className="github" src="src/assets/GitHub.png" alt="" />
-            <p className="text1">GitHub</p>
+            <p className="textSkills">GitHub</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Html" src="src/assets/Html.png" alt="" />
-            <p className="text1">HTML</p>
+            <p className="textSkills">HTML</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Css" src="src/assets/Css.png" alt="" />
-            <p className="text1">CSS</p>
+            <p className="textSkills">CSS</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Java" src="src/assets/Java.png" alt="" />
-            <p className="text1">JAVA</p>
+            <p className="textSkills">JAVA</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Js" src="src/assets/Javascript.png" alt="" />
-            <p className="text1">JAVASCRIPT</p>
+            <p className="textSkills">JAVASCRIPT</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="React" src="src/assets/React.png" alt="" />
-            <p className="text1">REACT</p>
+            <p className="textSkills">REACT</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Git" src="src/assets/Git.png" alt="" />
-            <p className="text1">GIT</p>
+            <p className="textSkills">GIT</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Docker" src="src/assets/Docker.png" alt="" />
-            <p className="text1">DOCKER</p>
+            <p className="textSkills">DOCKER</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Nodejs" src="src/assets/NodeJS.png" alt="" />
-            <p className="text1">NODE JS</p>
+            <p className="textSkills">NODE JS</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Mysql" src="src/assets/MySQL.png" alt="" />
-            <p className="text1">MY SQL</p>
+            <p className="textSkills">MY SQL</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Kotlin" src="src/assets/Kotlin.png" alt="" />
-            <p className="text1">KOTLIN</p>
+            <p className="textSkills">KOTLIN</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Figma" src="src/assets/figma.png" alt="" />
-            <p className="text1">FIGMA</p>
+            <p className="textSkills">FIGMA</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Trello" src="src/assets/trello.png" alt="" />
-            <p className="text1">TRELLO</p>
+            <p className="textSkills">TRELLO</p>
           </div>
-          <div className="im1">
+          <div>
             <img className="Postman" src="src/assets/Postman.png" alt="" />
-            <p className="text1">POSTMAN</p>
+            <p className="textSkills">POSTMAN</p>
           </div>
         </div>
       </div>
