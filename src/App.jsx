87347427318eeabf35a54d5bd875d1ede43bfa9d
JeysonGomez <jeysongomez17@hotmail.com>
2024-05-20 11:06:42 -0500

    Correciones JSX y CSS SocialNetworks

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import "./App.css";
 import SocialNetworks from "./components/SocialNetworks/SocialNetWorks";
 
 function App() {
-  return;
-  <>
-    <SocialNetworks />
-  </>;
+  return (
+    <>
+      <SocialNetworks />
+    </>
+  );
 }
 
 export default App;
