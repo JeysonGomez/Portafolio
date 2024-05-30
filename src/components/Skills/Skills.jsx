360c379ef8d4a0671724f3fd077e84161feec8e2
JeysonGomez <jeysongomez17@hotmail.com>
2024-05-30 10:15:34 -0500

    Modificaciones en etiqueta alt

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,68 +3,100 @@ import "./Skills.css";
 
 function Skills() {
   return (
-    <section className="generalContent bg1">
-      <div className="SkillsContent">
-        <div className="infoSkills">
+    <section className="generalContent allBackground">
+      <div>
+        <div className="informationSkills">
           <h1>Mis Habilidades en:</h1>
         </div>
 
         <div className="imgSkills">
           <div>
-            <img className="github" src="src/assets/GitHub.png" alt="GitHub" />
-            <p className="titleSkills">GitHub</p>
+            <img
+              className="github"
+              src="src/assets/GitHub.png"
+              alt="Imagen GitHub"
+            />
+            <p className="skillsTitle">GitHub</p>
           </div>
           <div>
-            <img className="Html" src="src/assets/Html.png" alt="HTML" />
-            <p className="titleSkills">HTML</p>
+            <img className="Html" src="src/assets/Html.png" alt="Imagen HTML" />
+            <p className="skillsTitle">HTML</p>
           </div>
           <div>
-            <img className="Css" src="src/assets/Css.png" alt="CSS" />
-            <p className="titleSkills">CSS</p>
+            <img className="Css" src="src/assets/Css.png" alt="Imagen CSS" />
+            <p className="skillsTitle">CSS</p>
           </div>
           <div>
-            <img className="Java" src="src/assets/Java.png" alt="Java" />
-            <p className="titleSkills">JAVA</p>
+            <img className="Java" src="src/assets/Java.png" alt="Imagen Java" />
+            <p className="skillsTitle">JAVA</p>
           </div>
           <div>
             <img
               className="Js"
               src="src/assets/Javascript.png"
-              alt="JavaScript"
+              alt="Imagen JavaScript"
             />
-            <p className="titleSkills">JAVASCRIPT</p>
+            <p className="skillsTitle">JAVASCRIPT</p>
           </div>
           <div>
-            <img className="React" src="src/assets/React.png" alt="React.Js" />
-            <p className="titleSkills">REACT JS</p>
+            <img
+              className="React"
+              src="src/assets/React.png"
+              alt=" Imagen React.Js"
+            />
+            <p className="skillsTitle">REACT JS</p>
           </div>
           <div>
-            <img className="Git" src="src/assets/Git.png" alt="Git" />
-            <p className="titleSkills">GIT</p>
+            <img className="Git" src="src/assets/Git.png" alt="Imagen Git" />
+            <p className="skillsTitle">GIT</p>
           </div>
           <div>
-            <img className="Docker" src="src/assets/Docker.png" alt="Docker" />
-            <p className="titleSkills">DOCKER</p>
+            <img
+              className="Docker"
+              src="src/assets/Docker.png"
+              alt="Imagen Docker"
+            />
+            <p className="skillsTitle">DOCKER</p>
           </div>
           <div>
-            <img className="Nodejs" src="src/assets/NodeJS.png" alt="Node js" />
-            <p className="titleSkills">NODE JS</p>
+            <img
+              className="Nodejs"
+              src="src/assets/NodeJS.png"
+              alt="Imagen Node js"
+            />
+            <p className="skillsTitle">NODE JS</p>
           </div>
           <div>
-            <img className="Mysql" src="src/assets/MySQL.png" alt="My SQL" />
-            <p className="titleSkills">MY SQL</p>
+            <img
+              className="Mysql"
+              src="src/assets/MySQL.png"
+              alt="Imagen My SQL"
+            />
+            <p className="skillsTitle">MY SQL</p>
           </div>
           <div>
-            <img className="Kotlin" src="src/assets/Kotlin.png" alt="Kotlin" />
-            <p className="titleSkills">KOTLIN</p>
+            <img
+              className="Kotlin"
+              src="src/assets/Kotlin.png"
+              alt="Imagen Kotlin"
+            />
+            <p className="skillsTitle">KOTLIN</p>
           </div>
           <div>
-            <img className="Figma" src="src/assets/figma.png" alt="Figmagit" />
-            <p className="titleSkills">FIGMA</p>
+            <img
+              className="Figma"
+              src="src/assets/figma.png"
+              alt="Imagen Figma"
+            />
+            <p className="skillsTitle">FIGMA</p>
           </div>
           <div>
-            <img className="Trello" src="src/assets/trello.png" alt="Trello" />
-            <p className="titleSkills">TRELLO</p>
+            <img
+              className="Trello"
+              src="src/assets/trello.png"
+              alt="Imagen Trello"
+            />
+            <p className="skillsTitle">TRELLO</p>
           </div>
           <div>
             <img
@@ -72,7 +104,7 @@ function Skills() {
               src="src/assets/Postman.png"
               alt="Postman"
             />
-            <p className="titleSkills">POSTMAN</p>
+            <p className="skillsTitle">POSTMAN</p>
           </div>
         </div>
       </div>
