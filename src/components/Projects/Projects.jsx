d026d326d6a71cbf036cbaca6c0e51875e5f6518
JeysonGomez <jeysongomez17@hotmail.com>
2024-06-01 09:10:10 -0500

    Correcion texto Proyectos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,99 +1,99 @@
 import React from "react";
-import "./Proyects.css";
+import "./Projects.css";
 
-function Proyects() {
+function Projects() {
   return (
-    <section className="wrapContent allBackground">
+    <section className="projectContent allBackground">
       <div className="proyectosContent">
-        <div className="infoProyectos">
+        <div className="infoProyects">
           <h1>Mis Proyectos</h1>
         </div>
-        <div className="imgProyectos">
-          <div className="imgP">
+        <div className="imgProyects">
+          <div className="imageProjects">
             <img
               className="Calculadora"
               src="src/assets/Proyectos/Calculadora.png"
-              alt=""
+              alt="Imagen calculadora"
             />
-            <h2 className="text2">Calculadora :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">Calculadora :</h2>
+            <p className="descriptionProject">
               Página desarrollada para realizar tipo de cálculos como sumas,
               restas, multiplicación, división, con una interfaz sencilla y muy
               sencilla de utilizar.
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
               className="Manage"
               src="src/assets/Proyectos/Manage.png"
-              alt=""
+              alt="Imagen Manage"
             />
-            <h2 className="text2">Mana Proyect :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">Mana Proyect :</h2>
+            <p className="descriptionProject">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
               className="PetShop"
               src="src/assets/Proyectos/PetShop.png"
-              alt=""
+              alt="Imagen PetShop"
             />
-            <h2 className="text2">PetShop Proyect :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">PetShop Proyect :</h2>
+            <p className="descriptionProject">
               Página desarrollada para tu destino en línea para todo lo
               ¡Relacionado con tus amigos peludos de cuatro patas! Entendemos
               que tus mascotas son más que solo animales; son parte de tu
               familia.
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
               className="Sportit"
               src="src/assets/Proyectos/Sportit.png"
-              alt=""
+              alt="Imagen SportIt"
             />
-            <h2 className="text2">SportIt Proyect :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">SportIt Proyect :</h2>
+            <p className="descriptionProject">
               Página desarrollada para todo lo relacionado con el mundo del
               deporte. Ya seas un fanático apasionado, un atleta dedicado o
               simplemente, este buscando mantenerte al día con las últimas
               noticias y eventos deportivos.
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
               className="Proyecto1"
               src="src/assets/Proyectos/Proyecto1.png"
-              alt=""
+              alt="Imagen Proyecto estudiantil"
             />
-            <h2 className="text2">Proyecto :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">Student project :</h2>
+            <p className="descriptionProject">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
-              className="Proyecto2"
+              className="Proyecto1"
               src="src/assets/Proyectos/Proyecto2.png"
-              alt=""
+              alt="Imagen Proyecto estudiantil"
             />
-            <h2 className="text2">Proyecto :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">Student project:</h2>
+            <p className="descriptionProject">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imgP">
+          <div className="imageProjects">
             <img
               className="Manage"
               src="src/assets/Proyectos/DOREMI.png"
-              alt=""
+              alt="Imagen DoReMi"
             />
-            <h2 className="text2">DoReMi Proyect :</h2>
-            <p className="descripcionP">
+            <h2 className="titleProject">DoReMi Proyect :</h2>
+            <p className="descriptionProject">
               Página desarrollada con el equipo de estudio, para explorar él
               ¡Maravilloso mundo de la música! Creemos que la música es más que
               solo sonido; es una forma de vida, una expresión de emociones y
