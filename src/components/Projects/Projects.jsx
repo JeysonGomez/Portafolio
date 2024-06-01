81407c94144ca869f5f48e11b4e0c130676c3a5e
JeysonGomez <jeysongomez17@hotmail.com>
2024-06-01 08:54:49 -0500

    Correcciones Ortograficas

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,101 +1,101 @@
 import React from "react";
-import "./Projects.css";
+import "./Proyects.css";
 
-function Projects() {
+function Proyects() {
   return (
-    <section className="ProjectsContent AllBackground">
-      <div className="projectsContent">
-        <div className="infoProjects">
+    <section className="wrapContent allBackground">
+      <div className="proyectosContent">
+        <div className="infoProyectos">
           <h1>Mis Proyectos</h1>
         </div>
-        <div className="imgProjects">
-          <div className="imageProjects">
+        <div className="imgProyectos">
+          <div className="imgP">
             <img
               className="Calculadora"
               src="src/assets/Proyectos/Calculadora.png"
-              alt="Proyecto de una Calculadora la cual funciona para realizar operaciones matematicas sencillas"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto Calculadora :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">Calculadora :</h2>
+            <p className="descripcionP">
               Página desarrollada para realizar tipo de cálculos como sumas,
               restas, multiplicación, división, con una interfaz sencilla y muy
               sencilla de utilizar.
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
               className="Manage"
               src="src/assets/Proyectos/Manage.png"
-              alt="Proyecto Pagina Manage web"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto Manage :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">Mana Proyect :</h2>
+            <p className="descripcionP">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
               className="PetShop"
               src="src/assets/Proyectos/PetShop.png"
-              alt="Proyecto pagina Petshop web, su funcionalidad es realizar pedidos de alimentos y objetos de entretenimiento a tus mascotas"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto PetShop :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">PetShop Proyect :</h2>
+            <p className="descripcionP">
               Página desarrollada para tu destino en línea para todo lo
               ¡Relacionado con tus amigos peludos de cuatro patas! Entendemos
               que tus mascotas son más que solo animales; son parte de tu
               familia.
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
               className="Sportit"
               src="src/assets/Proyectos/Sportit.png"
-              alt="Proyecto pagina de deportes web el cual muestra detalles de los deportes"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto SportIt :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">SportIt Proyect :</h2>
+            <p className="descripcionP">
               Página desarrollada para todo lo relacionado con el mundo del
               deporte. Ya seas un fanático apasionado, un atleta dedicado o
-              simplemente, estés buscando mantenerte al día con las últimas
-              noticias y eventos deportivos
+              simplemente, este buscando mantenerte al día con las últimas
+              noticias y eventos deportivos.
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
               className="Proyecto1"
               src="src/assets/Proyectos/Proyecto1.png"
-              alt="Pagina realizada en proyecto de estudio"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto Estudiantil :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">Proyecto :</h2>
+            <p className="descripcionP">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
-              className="Proyecto1"
+              className="Proyecto2"
               src="src/assets/Proyectos/Proyecto2.png"
-              alt="Pagina realizada en proyecto de estudio"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto Estudiantil :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">Proyecto :</h2>
+            <p className="descripcionP">
               Proyecto realizado en HTML y CSS en prácticas estudiantiles para
               reforzar lo aprendido durante el tiempo de estudio
             </p>
           </div>
-          <div className="imageProjects">
+          <div className="imgP">
             <img
-              className="DoReMi"
+              className="Manage"
               src="src/assets/Proyectos/DOREMI.png"
-              alt="Pagina diseñada para la solicitud de instrumentos musicales"
+              alt=""
             />
-            <h2 className="titleProject">Proyecto DoReMi :</h2>
-            <p className="descriptionProject">
+            <h2 className="text2">DoReMi Proyect :</h2>
+            <p className="descripcionP">
               Página desarrollada con el equipo de estudio, para explorar él
-              maravilloso mundo de la música! Creemos que la música es más que
+              ¡Maravilloso mundo de la música! Creemos que la música es más que
               solo sonido; es una forma de vida, una expresión de emociones y
               una conexión universal que une a personas de todas las edades y
               culturas.
